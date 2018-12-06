fa18916513646d33e0e823a713ad079c635a18f0
marie <dvorzak.marie@gmx.at>
2018-12-06 20:22:08 +0000

    updated geometry to extruded curves

--- DAT505_project_dvorzak/js/index.js
+++ DAT505_project_dvorzak/js/index.js
@@ -8,9 +8,10 @@ var config = {
     waveDepth: 0.01
 };
 
-var renderer, scene, camera,warpVector,orientationCylinder,
-    frequencyData, audio, analyser, plane, curveObject, geometry, material
+var renderer, scene, camera,warpVector,orientationCylinder,extrudePath,
+    frequencyData, audio, analyser, plane, curveObject, geometry, material, points,pipeSpline, direction
 ;
+
 var time = 0;
 var lastPoint = 0;
 var frequencyCounter = 0;
@@ -24,7 +25,7 @@ function init() {
     scene = new THREE.Scene();
     var W = window.innerWidth,
         H = window.innerHeight;
-    camera = new THREE.PerspectiveCamera(45, W / H, .1, 1000);
+    camera = new THREE.PerspectiveCamera(45, W / H, .1, 800);
     camera.position.set(0, 55, 85);
     camera.lookAt(scene.position);
     var ambientLight = new THREE.AmbientLight(0xb3f442, 1.2);
@@ -49,53 +50,127 @@ function init() {
 
     console.log(group.children[9])
     scene.add(group);
-    group.position.set(-300, -30, 20)
+    group.position.set(-90, 0, 100)
 
     document.body.appendChild(renderer.domElement);
 }
 
+// function loadBasicSurface() {
+//     var cylinderGeometry = new THREE.CylinderGeometry( 10, 10, 40, 20,20 );
+//     var cylinderMaterial = new THREE.MeshBasicMaterial({
+//         color: 0xf2f2f2,
+//         vertexColors: THREE.FaceColors,
+//         side: THREE.DoubleSide,
+//         wireframe: true,
+//         specular: 1.5,
+//         shading: THREE.FlatShading,
+//     });
+//     orientationCylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+//     orientationCylinder.position.set(0,0,-25);
+//     // scene.add(orientationCylinder);
+//
+//
+//     var geometry = new THREE.CylinderGeometry( 4, 4, 200, 15,3 );//new THREE.BoxGeometry(10, 580, 30);
+//     var material = new THREE.MeshPhongMaterial({
+//         color: 0xf2f2f2,
+//         vertexColors: THREE.FaceColors,
+//         side: THREE.DoubleSide,
+//         specular: 0x050505,
+//         shininess: 100,
+//         // shading: THREE.FlatShading,
+//         // wireframe: true,
+//         wireframeLinewidth: 1,
+//     });
+//     plane = new THREE.Mesh(geometry, material);//150, 300, Math.PI/2, Math.PI, 25, 25
+//     plane.rotation.x += 2.9; //2.5 flach 1.5 winkel 1 flach horizontal
+//     plane.position.set(counter, -400, -700);
+//     plane.receiveShadow = true;
+//     plane.sortParticles = true;
+//     plane.castShadow = true;
+//     plane.geometry.dynamic = true;
+//     plane.material.needsUpdate = true;
+//     plane.geometry.colorsNeedUpdate = true;
+//     warpVector = new THREE.Vector3(0, 50, 0); //50
+//     group.add(plane);
+//     counter += 20;
+//     distortGeometry();
+// }
+
+function GrannyKnot() {
+    pipeSpline = new THREE.CatmullRomCurve3( [
+        new THREE.Vector3( 0, -10, 0 ),
+        new THREE.Vector3( 0, -9,0 ),
+        new THREE.Vector3( 0, -7,0 ),
+        new THREE.Vector3( 0, -5,0 ),
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( 0, 5, 0),
+        new THREE.Vector3( 0, 7, 0),
+        new THREE.Vector3( 0, 10, 0 )
+        // new THREE.Vector3( 0, 0, 40 )
+    ] );
+}
 function loadBasicSurface() {
-    var cylinderGeometry = new THREE.CylinderGeometry( 10, 10, 40, 20,20 );
-    var cylinderMaterial = new THREE.MeshBasicMaterial({
-        color: 0xf2f2f2,
-        vertexColors: THREE.FaceColors,
-        side: THREE.DoubleSide,
-        wireframe: true,
-        specular: 1.5,
-        shading: THREE.FlatShading,
-    });
-    orientationCylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-    orientationCylinder.position.set(0,0,-25);
-    // scene.add(orientationCylinder);
 
+    GrannyKnot();
+    extrudePath = pipeSpline;
+    var params = {
+        scale: 10,
+        extrusionSegments: 50,
+        radiusSegments: 12,
+    };
+    var tubeGeometry = new THREE.TubeBufferGeometry( extrudePath, params.extrusionSegments, 2, params.radiusSegments );
+    tubeGeometry.vertices = pipeSpline.getPoints( 50 );
 
-    var geometry = new THREE.CylinderGeometry( 10, 10, 280, 15,3 );//new THREE.BoxGeometry(10, 580, 30);
     var material = new THREE.MeshPhongMaterial({
-        color: 0xf2f2f2,
-        vertexColors: THREE.FaceColors,
+        color: 0x2d2d2d,
+        // vertexColors: THREE.FaceColors,
         side: THREE.DoubleSide,
-        specular: 0x050505,
+        specular: 0x2d2d2d,
         shininess: 100,
-        // shading: THREE.FlatShading,
-        // wireframe: true,
         wireframeLinewidth: 1,
     });
-    plane = new THREE.Mesh(geometry, material);
+    plane = new THREE.Mesh(tubeGeometry, material);//150, 300, Math.PI/2, Math.PI, 25, 25
+    plane.curve = pipeSpline;
     plane.rotation.x += 2.9; //2.5 flach 1.5 winkel 1 flach horizontal
-    plane.position.set(counter, -400, -700);
+    plane.position.set(counter, -100, -200);
     plane.receiveShadow = true;
     plane.sortParticles = true;
     plane.castShadow = true;
     plane.geometry.dynamic = true;
+    // plane.geometry.attributes.color.needsUpdate = true;
+
     plane.material.needsUpdate = true;
     plane.geometry.colorsNeedUpdate = true;
+    // plane.geometry.attributes.color.needsUpdate = true;
+
     warpVector = new THREE.Vector3(0, 50, 0); //50
     group.add(plane);
-    counter += 20;
+    counter += 7;
 
 }
+function distortGeometry() {
+    var width = 200 * 2 * Math.PI;
+    // var geom = new THREE.CylinderGeometry(width, height, horizontalSegments, verticalSegments);
+    var index = 0;//new THREE.CylinderGeometry( 4, 4, 200, 15,3 )
 
+    for(var i=0; i<=plane.geometry.vertices.length; i++) {
+            let t= 10;
+            var angle = t * 2.0 * Math.PI;
+/*
+  float angle = t * 2.0 * PI;
+  vec2 rot = vec2(cos(angle), sin(angle));
+  float z = t * 2.0 - 1.0;
+ */
+            if(plane.geometry.vertices[index]){
+            // plane.geometry.vertices[index].z = t*2.0*Math.PI;
+            plane.geometry.vertices[index].x = Math.cos(angle);
+            // plane.geometry.vertices[index].y = Math.sin(angle);
+            index++;
+            }
+    }
 
+    return plane;
+}
 function createCurves() {
     var curve = new THREE.CatmullRomCurve3([
         new THREE.Vector3(-10, 0, 10),
@@ -125,63 +200,59 @@ function createCurves() {
 
 // group.children[0].geometry.attributes.position.array[0]
 // group.children[0].curve
-function mapAudioInformation(ts) {
+function mapAudioInformation() {
 
     for (let i = 0; i < planeVertices; i++) {
         if (frequencyData[i] > 0) {
-            group.children[i].scale.z = frequencyData[i] / 70;
+            group.children[i].scale.y = frequencyData[i] / 60;
+            group.children[i].scale.z = frequencyData[i] / 80;
+            group.children[i].scale.x = frequencyData[i] / 120;
             if(frequencyData[i] > 50){
                 let updateColor = new THREE.Color("rgb("+30+"%, "+80+"%, "+(frequencyData[i])+"%)");
                 group.children[i].material.color = updateColor;
-                frequencyCounter++;
             }
             if(frequencyData[i] > 100){
                 let updateColor = new THREE.Color("rgb("+0+"%, "+(frequencyData[i])+"%, "+(frequencyData[i])+"%)");
                 group.children[i].material.color = updateColor;
-                frequencyCounter++;
             }
             if(frequencyData[i] > 200){
                 let updateColor = new THREE.Color("rgb("+90+"%, "+80+"%, "+190+"%)");
                 group.children[i].material.color = updateColor;
-                frequencyCounter++;
             }
         }
 
     }
 
-    // for (var i = 0, l = group.children[0].geometry.vertices.length; i<l; i+=3) {
-    //
-    //     group.children[0].geometry.vertices[i].x += -10 +orientationCylinder.geometry.vertices[i].x;
-    //     group.children[0].geometry.vertices[i].y += -10 +orientationCylinder.geometry.vertices[i].y;
-    //     group.children[0].geometry.vertices[i].z += -10 +orientationCylinder.geometry.vertices[i].z;
-    //     // console.log(orientationCylinder.geometry.vertices[i].x);
-    // }
-
-    // group.forEach(function(c, i) {
-    //     //frequency       //distance          //amplitude
-    //     c.scale.y = Math.sin(ts/1900*Math.PI + c.position.x*8.95 + c.position.z/10) + 1;
-    // });
     for (let i = 0; i < group.children.length; i++) {
-       group.children[i].scale.y = Math.cos(ts/190000*Math.PI + group.children[i].position.y*frequencyData[i]/35000 + group.children[i].position.y/10) + 1;
-        // var waveToUse = group.children[i].scale.y = Math.sin(ts/1900*Math.PI + group.children[i].position.x*8.95 + group.children[i].position.z/10) + 1;
-
+       // group.children[i].scale.y = Math.cos(ts/190000*Math.PI + group.children[i].position.y*frequencyData[i]/35000 + group.children[i].position.y/10) + 1;
     }
-    for (let i = 0; i < group.children[0].geometry.vertices.length; i++) {
-
-        group.children[0].geometry.vertices[i] = orientationCylinder.geometry.vertices[5].x;
-        group.children[0].geometry.verticesNeedUpdate = true;
-
-
-        group.children[0].geometry.verticesNeedUpdate = true;
+    let countBeats=0;
+    for (var i = 0, l = group.children.length; i < l; i++) {
+        var curveLine = group.children[i];
 
 
-        // group.children[0].geometry.attributes.position.needsUpdate = true;
+            (frequencyCounter === 16) ? direction = 1 : direction = -1;
+            (frequencyCounter === 16) ? frequencyCounter= 0 : frequencyCounter=frequencyCounter;
+        // (frequencyCounter === 16) ? console.log("change 1") : console.log("change to negative -1");
+            curveLine.curve.points[0].x += (frequencyData[i]/800)*direction;
+            curveLine.curve.points[1].x += (frequencyData[i-1]/800)*direction;
+            curveLine.curve.points[2].x += (frequencyData[i-2]/800)*direction;
+            curveLine.curve.points[3].x += (frequencyData[i-3]/800)*direction;
+            curveLine.curve.points[4].x += (frequencyData[i-4]/800)*direction;
+        // console.log(frequencyCounter)
+            frequencyCounter++;
 
-        // const dist = v.distanceTo(warpVector);
-        // let waveToUse = (10 + (waveDepth) * Math.sin(dist / -frequenz + (time/speed*magnitude))) * radius;
-        //
-        // v.normalize().multiplyScalar(waveToUse);
 
+        if(frequencyData[i]> 100){
+            // curveLine.curve.points[1].x = frequencyData[i];
+        }
+        curveLine.geometry.vertices = curveLine.curve.getPoints( 10 );
+        curveLine.geometry.verticesNeedUpdate = true;
+        if(frequencyData[0]> 100) {
+            setTimeout(function(){
+                group.children[i].geometry = new THREE.TubeBufferGeometry(extrudePath, 12, 2, 20);
+            },1/30);
+        }
     }
 
     group.children[0].geometry.computeVertexNormals();
